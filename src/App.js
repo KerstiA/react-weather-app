0c6f90209e8ff771a04d3da229dbe3b590dbf9cc
KerstiA <74317766+KerstiA@users.noreply.github.com>
2021-01-23 12:32:16 +0200

    Added h1

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather
-      <footer>
-        This project is coded by Kersti Allikvee and is {""}
-        <a href="https://github.com/KerstiA/react-weather-app" target="_blank">
-          open-source on GitHub
-        </a>
-      </footer>
+      <div className="conatiner">
+        <h1>Weather</h1>
+        <footer>
+          This project is coded by Kersti Allikvee and is {""}
+          <a
+            href="https://github.com/KerstiA/react-weather-app"
+            target="_blank"
+          >
+            open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
