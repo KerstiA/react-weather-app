b2f5a4da024664ad9cc097d6e7f10184c9e6179d
KerstiA <74317766+KerstiA@users.noreply.github.com>
2021-01-23 11:24:55 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      Weather
+      <footer>
+        This project is coded by Kersti Allikvee and is {""}
+        <a href="https://github.com/KerstiA/react-weather-app" target="_blank">
+          open-source on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
