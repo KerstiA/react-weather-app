b02ebdcf87c28831e3d249394f2d8242e5b9593a
KerstiA <74317766+KerstiA@users.noreply.github.com>
2021-01-25 00:58:38 +0200

    added 5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -11,15 +11,19 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
-  if (loaded) {
+  if (loaded && props.city === forecast.city.name) {
     return (
       <div className="WeatherForecast">
         <WeatherForecastPreview data={forecast.list[0]} />
+        <WeatherForecastPreview data={forecast.list[9]} />
+        <WeatherForecastPreview data={forecast.list[18]} />
+        <WeatherForecastPreview data={forecast.list[27]} />
+        <WeatherForecastPreview data={forecast.list[36]} />
       </div>
     );
   } else {
     let apiKey = `88493d926515e36fa055dfe27bbb8ecd`;
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}`;
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(handleForecastResponse);
 
     return null;
--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,25 +1,31 @@
 import React from "react";
 
 export default function WeatherForecastPreview(props) {
-  function hours() {
+  function day() {
     let date = new Date(props.data.dt * 1000);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[date.getDay()];
 
-    return `${hours}`;
+    return `${day}`;
   }
 
   function temperature() {
-    let temperature = Math.round(props.data.main.temp);
-    return `${temperature}°C`;
+    let maxTemp = Math.round(props.data.main.temp_max);
+    let minTemp = Math.round(props.data.main.temp_min);
+    return `Max ${maxTemp}°C / Min ${minTemp}°C`;
   }
 
+  function wind() {
+    let wind = Math.round(props.data.wind.speed);
+    return `${wind}`;
+  }
   return (
     <div className="WeatherForecast">
-      {hours()}
+      {day()}
+      <br />
       {temperature()}
+      <br />
+      Wind: {wind()}km/h
     </div>
   );
 }
